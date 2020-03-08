e19099cc657dc8a05fb753d3fbf17f2bb21a2306
nattachai <nattachai6317@gmail.com>
2020-03-08 23:55:32 +0700

    first commit

---
+++ src/main/java/com/arm/react/AllLinkReactApplication.java
@@ -0,0 +1,13 @@
+package com.arm.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllLinkReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllLinkReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arm/react/AllLinkReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arm.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllLinkReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
